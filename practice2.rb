fbd3f4fb8dfdbe2e603300d4bb87af6605c98939
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-06 13:54:35 -0500

    practice stuff

--- practice2.rb
+++ practice2.rb
@@ -1,4 +1,22 @@
-print "What is your name? "
-name = gets.chomp
+animal_crackers = [
+  "bunny",
+  "giraffe",
+  "lion",
+  "monkey"
+]
 
-puts "..." + name.upcase + "..."
+animal_crackers.each do |animal|
+  puts "A dinosaur has bitten the #{animal}'s head off!"
+end
+
+puts "The bag of groceries fell on the floor!"
+
+food = ["fruit", "vegetables", "dog food", "bread"]
+
+food.each do |food_eaten|
+  puts "The dog has eaten the #{food_eaten}!"
+end
+
+4.times { |x| puts "I'm printing something from a block, and x is #{x}!"}
+
+["a","b","c","d"].each { |item| puts "Now the block parameter has a value of the current array item: #{item}"}
