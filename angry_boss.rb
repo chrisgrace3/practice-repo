1bf24e44273b7dbabc96d06a16560304cee89164
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-10 17:41:08 -0500

    um

---
+++ angry_boss.rb
@@ -0,0 +1,6 @@
+puts "***Begin Angry Boss Program v.2.3**"
+print "Please enter your last name: "
+last_name = gets.chomp.capitalize
+puts 'WHAT DO YOU WANT, ' + last_name.upcase + '??'
+want = gets.chomp
+puts 'WHADDAYA MEAN, "' + want.upcase + '"?!? YOU\'RE FIRED!!' 
