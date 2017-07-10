a86269cc1636985a7e4a3b767e659b3f2e5386cd
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-10 15:31:36 -0500

    clean up code in better number ruby script

--- better-number.rb
+++ better-number.rb
@@ -1,8 +1,7 @@
 puts "What's your favorite number?"
 fav_num_string = gets.chomp
-fav_num_integer = fav_num_string.to_i
-better_num_integer = fav_num_integer.to_i + 1
-better_num_string = better_num_integer.to_s
+better_num_integer = fav_num_string.to_i + 1
+
 
 puts fav_num_string + ' is a pretty good number. There are some better numbers out there,
-though. May I suggest ' + better_num_string + ' instead?'
+though. May I suggest ' + better_num_integer.to_s + ' instead?'
--- hash-practice.rb
+++ hash-practice.rb
@@ -1,3 +1,8 @@
-application = {name: "Chris Grace", age: "immortal", location: "Earth"}
+#application = {name: "Chris Grace", age: "immortal", location: "Earth"}
 
-p application[:name]
+# p application[:name]
+
+var1 = "hello world"
+var1 = var1.upcase_at(0)
+
+p var1
